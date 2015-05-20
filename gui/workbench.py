a5ff57b09ec3bca15041ac8f070ecdc1180ff112
kaktus3000 <insi@localhost.localdomain>
2015-05-20 21:50:41 +0200

    added comments and modal dialog

--- gui/workbench.py
+++ gui/workbench.py
@@ -8,20 +8,21 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 
+#configure root window
 root = tk.Tk()
 root.title("Edamni")
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 root.geometry('{}x{}'.format(600, 450) )
 
+#configure master frame
 windowFrame = tk.Frame(root)
 windowFrame['background'] = '#e00'
 windowFrame.grid(sticky=tk.N+tk.E+tk.S+tk.W)
 windowFrame.columnconfigure(1, weight=1)
 windowFrame.rowconfigure(0, weight=1)
 
-lModes = ['Speakers', 'Acoustic Circuit', 'Simulation', 'Results']
-
+#frame for radio buttons
 modeSelectionFrame = ttk.Frame(windowFrame)
 modeSelectionFrame['borderwidth'] = 2
 modeSelectionFrame['relief'] = 'solid'
@@ -29,13 +30,17 @@ modeSelectionFrame.grid(sticky=tk.N)
 
 ttk.Label(modeSelectionFrame, text="Mode").grid()
 
+#radio button variable
 mode = tk.StringVar()
 
-#create radio buttons for the simulation elements
+#modes for the gui to be in
+lModes = ['Speakers', 'Acoustic Circuit', 'Simulation', 'Results']
 
+#create radio buttons for mode setting
 for strMode in lModes:
     ttk.Radiobutton(modeSelectionFrame, text=strMode, variable=mode, value=strMode).grid()
 
+#create frame for contents of program
 mainFrame = tk.Frame(windowFrame)
 mainFrame['background'] = '#0e0'
 mainFrame['borderwidth'] = 2
@@ -45,21 +50,22 @@ mainFrame.grid(row=0, column=1, sticky=tk.N+tk.E+tk.S+tk.W)
 mainFrame.rowconfigure(0, weight=1)
 mainFrame.columnconfigure(0, weight=1)
 
+#create frame for mode speaker list
 speakerFrame = tk.Frame(mainFrame)
 speakerFrame['background'] = '#00e'
+speakerFrame.rowconfigure(1, weight=1)
+speakerFrame.columnconfigure(0, weight=1)
 
-buttonFrame = ttk.Frame(speakerFrame)
-buttonFrame.grid(sticky=tk.W)
+#frame for buttons
+speakerButtonFrame = ttk.Frame(speakerFrame)
+speakerButtonFrame.grid(sticky=tk.W)
 
+#list for speakers
 speakerListBox = tk.Listbox(speakerFrame)
 speakerListBox['selectmode'] = tk.SINGLE
 speakerListBox.grid(sticky=tk.N+tk.S+tk.W+tk.E)
 
-speakerFrame.rowconfigure(1, weight=1)
-speakerFrame.columnconfigure(0, weight=1)
-
-
-
+#callbacks for buttons
 def addSpeaker(*args):
 	speakerFileName = askopenfilename(initialdir="../preprocessor/bcd")
 	speakerListBox.insert(tk.END, speakerFileName)
@@ -94,18 +100,47 @@ def moveSpeakerDown(*args):
 	speakerListBox.insert(iSelection + 1, strContent)
 	speakerListBox.selection_set(iSelection + 1)
 
-tk.Button(buttonFrame, bitmap="@xbm/add.xbm", command=addSpeaker).grid(row=1, column=0)
-tk.Button(buttonFrame, bitmap="@xbm/remove.xbm", command=removeSpeaker).grid(row=1, column=1)
-tk.Button(buttonFrame, bitmap="@xbm/up.xbm", command=moveSpeakerUp).grid(row=1, column=2)
-tk.Button(buttonFrame, bitmap="@xbm/down.xbm", command=moveSpeakerDown).grid(row=1, column=3)
+#create buttons
+tk.Button(speakerButtonFrame, bitmap="@xbm/add.xbm", command=addSpeaker).grid(row=1, column=0)
+tk.Button(speakerButtonFrame, bitmap="@xbm/remove.xbm", command=removeSpeaker).grid(row=1, column=1)
+tk.Button(speakerButtonFrame, bitmap="@xbm/up.xbm", command=moveSpeakerUp).grid(row=1, column=2)
+tk.Button(speakerButtonFrame, bitmap="@xbm/down.xbm", command=moveSpeakerDown).grid(row=1, column=3)
 
-
+#create frame for mode acoustic circuit
 acuCircuitFrame = ttk.Frame(mainFrame)
-ttk.Button(acuCircuitFrame, text="Add Element", command=exit).grid()
+acuCircuitFrame.rowconfigure(1, weight=1)
+acuCircuitFrame.columnconfigure(0, weight=1)
 
+#frame for buttons
+acuButtonFrame = tk.Frame(acuCircuitFrame)
+acuButtonFrame['background'] = '#770'
+acuButtonFrame.grid(sticky=tk.W+tk.E)
+
+#canvas for circuit elements
+acuCanvas = tk.Canvas(acuCircuitFrame)
+acuCanvas['background'] = '#070'
+acuCanvas.grid(row=1, column=0, sticky=tk.N+tk.S+tk.W+tk.E)
+
+#callbacks for buttons
+def addAcousticElement(*args):
+	print("adding element...")
+	addElementDialog = tk.Toplevel(root)
+	addElementDialog.transient(root)
+	addElementDialog.grab_set()
+	addElementDialog.wm_title("Add Acoustic Element")
+
+	tk.Button(addElementDialog, text="Conical", bitmap="@xbm/conical.xbm", command=lambda:addElementDialog.destroy()).grid()
+	tk.Button(addElementDialog, text="OK", command=lambda:addElementDialog.destroy()).grid()
+	root.wait_window(addElementDialog)
+
+#create buttons
+tk.Button(acuButtonFrame, bitmap="@xbm/add.xbm", command=addAcousticElement).grid(row=1, column=0)
+
+#create frame for mode simulation
 simuFrame = ttk.Frame(mainFrame)
 ttk.Button(simuFrame, text="Run Simulation", command=exit).grid()
 
+#create frame for mode results
 resultsFrame = ttk.Frame(mainFrame)
 choiceLabel = ttk.Label(resultsFrame, text='xxx dB')
 choiceLabel.grid()
@@ -119,7 +154,9 @@ def onChangeType(*args):
     
     dModeFrames[mode.get()].grid(sticky=tk.N+tk.E+tk.S+tk.W)
 
+#initialize to speaker mode
 mode.trace("w", onChangeType)
 mode.set(lModes[0])
 
+#start the whole thing
 root.mainloop()
