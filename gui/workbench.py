bb59a22358d8eafae701b1a646edfe3901f73f17
kaktus3000 <insi@localhost.localdomain>
2015-05-24 20:52:54 +0200

    started circuit panel

--- gui/workbench.py
+++ gui/workbench.py
@@ -38,7 +38,7 @@ lModes = ['Speakers', 'Acoustic Circuit', 'Simulation', 'Results']
 
 #create radio buttons for mode setting
 for strMode in lModes:
-    ttk.Radiobutton(modeSelectionFrame, text=strMode, variable=mode, value=strMode).grid()
+	ttk.Radiobutton(modeSelectionFrame, text=strMode, variable=mode, value=strMode).grid()
 
 #create frame for contents of program
 mainFrame = tk.Frame(windowFrame)
@@ -121,6 +121,35 @@ acuCanvas = tk.Canvas(acuCircuitFrame)
 acuCanvas['background'] = '#070'
 acuCanvas.grid(row=1, column=0, sticky=tk.N+tk.S+tk.W+tk.E)
 
+class MovableHandler:
+	def __init__(self, canvas, widget, canvasID):#, canvas, canvasID):
+		self.m_Canvas = canvas
+
+		self.moving_widget = canvasID
+		self.moving_x = 0
+		self.moving_y = 0
+
+		widget.bind("<ButtonPress-1>", self.onMovablePress)
+		widget.bind("<ButtonRelease-1>", self.onMovableRelease)
+
+	def onMovablePress(self, event):
+#		self.moving_widget = self.m_Canvas.find_closest(event.x, event.y)[0]
+		self.moving_x = event.x
+		self.moving_y = event.y
+
+	def onMovableRelease(self, event):
+		dest_x = event.x
+		dest_y = event.y
+
+		delta_x = dest_x - self.moving_x
+		delta_y = dest_y - self.moving_y
+
+		self.m_Canvas.move(self.moving_widget, delta_x, delta_y)
+
+#		self.moving_widget = None
+		self.moving_x = 0
+		self.moving_y = 0
+
 #callbacks for buttons
 def addAcousticElement(*args):
 	print("adding element...")
@@ -129,12 +158,75 @@ def addAcousticElement(*args):
 	addElementDialog.grab_set()
 	addElementDialog.wm_title("Add Acoustic Element")
 
-	tk.Button(addElementDialog, text="Conical", bitmap="@xbm/conical.xbm", command=lambda:addElementDialog.destroy()).grid()
-	tk.Button(addElementDialog, text="OK", command=lambda:addElementDialog.destroy()).grid()
+	#modes for the gui to be in
+	lElementTypes = ['Conical', 'Exponential', 'Mic', 'Open', 'Speaker', 'Wall']
+
+	elemType = tk.StringVar()
+
+	#create radio buttons for mode setting
+	for iElementType in range(len(lElementTypes) ):
+		strElemType = lElementTypes[iElementType]
+		ttk.Radiobutton(addElementDialog, text=strElemType, variable=elemType, value=strElemType).grid(row = 1, column = iElementType)
+		tk.Label(addElementDialog, bitmap="@xbm/" + strElemType.lower() + "0.xbm").grid(row=0, column = iElementType)
+
+	def addElementToCanvas(*args):
+		print("element type is " + elemType.get())
+		elementFrame = ttk.Frame(acuCircuitFrame)
+
+		canvasID = acuCanvas.create_window(100, 100, window=elementFrame, tags="movable")
+
+		movingLabel = tk.Label(elementFrame, bitmap="@xbm/" + elemType.get().lower() + "0.xbm")
+		movingLabel.grid(row = 1, column = 1)
+		tk.Button(elementFrame, bitmap="@xbm/connector.xbm", command=exit).grid(row = 1, column = 0)
+		tk.Button(elementFrame, bitmap="@xbm/connector.xbm", command=exit).grid(row = 1, column = 2)
+
+		myHandler = MovableHandler(acuCanvas, movingLabel, canvasID)
+
+		addElementDialog.destroy()
+
+	#add button to create element
+	tk.Button(addElementDialog, text="OK", command=addElementToCanvas).grid()
+	#add button to cancel creation
+	tk.Button(addElementDialog, text="Cancel", command=lambda:addElementDialog.destroy()).grid()
+
+
 	root.wait_window(addElementDialog)
 
 #create buttons
 tk.Button(acuButtonFrame, bitmap="@xbm/add.xbm", command=addAcousticElement).grid(row=1, column=0)
+
+#put up a window to modify element options
+
+#dictionary with element options
+dElements = dict(
+	conical = [('length', 'm'),
+			   ('damping constant', '1'),
+			   ('A1', 'm^2'),
+			   ('A2', 'm^2')],
+	
+	expo = [('length', 'm'),
+			('A1', 'm^2'),
+			('A2', 'm^2'),
+			('exponent', '1')],
+	 
+	wall = [('damping thickness', 'm'),
+			('damping transient', 'm'),
+			('damping constant', 'Ns/m^4')],
+	 
+	space = [('length', 'm'),
+			 ('fraction', '1')],
+	
+	speaker = [('index', '1')],
+	
+	fork = [('A1', 'm^2'),
+			('A2', 'm^2'),
+			('A3', 'm^2')],
+
+	mic = []
+	)
+
+#create frame for element options
+
 
 #create frame for mode simulation
 simuFrame = ttk.Frame(mainFrame)
@@ -147,15 +239,15 @@ choiceLabel.grid()
 
 dModeFrames = {'Speakers': speakerFrame, 'Acoustic Circuit': acuCircuitFrame, 'Simulation': simuFrame, 'Results': resultsFrame}
 
-def onChangeType(*args):
+def onChangeMode(*args):
 	#hide all other frames, show selected
-    for subFrame in dModeFrames.values():
-        subFrame.grid_remove()
-    
-    dModeFrames[mode.get()].grid(sticky=tk.N+tk.E+tk.S+tk.W)
+	for subFrame in dModeFrames.values():
+		subFrame.grid_remove()
+	
+	dModeFrames[mode.get()].grid(sticky=tk.N+tk.E+tk.S+tk.W)
 
 #initialize to speaker mode
-mode.trace("w", onChangeType)
+mode.trace("w", onChangeMode)
 mode.set(lModes[0])
 
 #start the whole thing
