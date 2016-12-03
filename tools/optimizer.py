1b8d0560a6d05887ffa120a6b28f67b4216e77b0
insi <insi@localhost.localdomain>
2016-12-03 14:21:27 +0100

    started optimizer

--- tools/optimizer.py
+++ tools/optimizer.py
@@ -2,12 +2,19 @@
 import configparser
 import xml.etree.ElementTree as ET
 import sys
+import os
 
 # optimize horn geometry with regard to objective function
 
 #read optimizer input file
-strConfig = sys.argv[1]
-strOptimizeInput = sys.argv[2]
+strOptimizeInput = sys.argv[1]
+
+strOSDir = os.path.dirname(strOptimizeInput)
+
+if strOSDir=="":
+	strOSDir = "."
+
+g_strDir = strOSDir + "/"
 
 config = configparser.ConfigParser()
 config.read(strOptimizeInput)
@@ -16,12 +23,14 @@ g_strOptimizerIni = config.get("optimize_input", "optimizer_ini")
 g_strMaterialCostIni = config.get("optimize_input", "material_cost_ini")
 g_strSPLCostIni = config.get("optimize_input", "spl_cost_ini")
 g_strHornFile = config.get("optimize_input", "horn_file")
-g_strHornOptimizerFile = config.get("optimize_input", "horn_optimizer_file")
+#g_strHornOptimizerFile = config.get("optimize_input", "horn_optimizer_file")
 
 #read optimizer input
+strOptimIniFile = g_strDir + g_strOptimizerIni
+print("optimizer: opening optimizer ini from", strOptimIniFile)
 
 optimizerConfig = configparser.ConfigParser()
-optimizerConfig.read(g_strOptimizerIni)
+optimizerConfig.read(strOptimIniFile)
 
 #define initial step widths
 g_fMinLenChange = float(optimizerConfig.get("step_control", "min_len_change"))
@@ -36,42 +45,91 @@ g_nCPUs = optimizerConfig.get("runtime_control", "cpus")
 tree = ET.parse(g_strHornFile)
 g_Horn = tree.getroot()
 
-g_CurrParams = dict()
+PARAM_MIN = 0
+PARAM_MAX = 1
+PARAM_CURR = 2
+PARAM_MIN_STEP = 3
+PARAM_MIN_CHANGE = 4
+# [min, max, current]
+g_dParams = dict()
 
 for section in g_Horn:
 	sectionID = section.attrib["id"]
 
 	for elem in section:
 		strTag = elem.tag
-		if strTag == "a1" or strTag == "a2" or strTag == "a3" or strTag == "length":
-			g_CurrParams[sectionID + "." + strTag] = float(elem.text)
+		
+		if("min" in elem.attrib.keys()):
+			fMinStep = g_fMinLenStep
+			fMinChange = g_fMinLenChange
+			if elem.tag == "a1" or elem.tag == "a2" or elem.tag == "a3":
+				fMinChange = g_fMinAreaChange
+				fMinStep = 0
 
-print(g_CurrParams)
+			g_dParams[sectionID + "." + strTag] = [float(elem.attrib["min"]), float(elem.attrib["max"]), float(elem.text), fMinStep, fMinChange]
 
+def writeModifiedXML(params, strModifiedXML):
+
+	for strParam in params.keys():
+		astrSplit = strParam.split('.')
+		strSectionID = astrSplit[0]
+		strSectionParam = astrSplit[1]
+		for section in g_Horn:
+			sectionID = section.attrib["id"]
+			if sectionID == strSectionID:
+				for elem in section:
+					if elem.tag == strSectionParam:
+						print("bla.")
+						# replace value in XML structure
+						elem.text = str(params[strParam][PARAM_CURR])
+
+	# write modified XML structure
+	print("writing modified XML to", strModifiedXML)
+	
+	with open(strModifiedXML, 'wb') as f:
+		f.write(bytes('<?xml version="1.0" encoding="UTF-8" ?>', 'utf-8'))
+		tree.write(f, 'utf-8')
 
 def evaluate(params):
-	#define new geometry
+	# define new geometry
 	
+	# find horn section id
 
-	#check for constant cross section in fork elements
-	#A1 = A2 + A3
+	# check for constant cross section in fork elements
+	# A1 = A2 + A3
 
-	#check for neighboring cross-sections
-	#usually we don't want hard steps at the element borders
+	# check for neighboring cross-sections
+	# usually we don't want hard steps at the element borders
 
 
-	#check if we already have a similar design in our cache
-	#if we do, discard this design and try something different
+	# check if we already have a similar design in our cache
+	# if we do, discard this design and try something different
 
-	#approximate is within global tolerances
+	# approximate means within global tolerances
 	
 	#cache hit: we ain't got time for that
-	print(params)
-	return float("inf")
+	#print(params)
+	fReturnValue = 0
+	for strParam in params.keys():
+		fMin = params[strParam][PARAM_MIN]
+		fMax = params[strParam][PARAM_MAX]
+		fRange = fMax - fMin
+		fMean = .5 * (fMax + fMin)
+		fDist = params[strParam][PARAM_CURR] - fMean
+		fRatio = fDist / fRange
+		fReturnValue += fRatio ** 2
+	
+	print(fReturnValue)
+
 	
 	#write horn definition xml file
+	strModifiedXML = "optim_out.xml"
+	writeModifiedXML(params, strModifiedXML)
+	
+	return fReturnValue
 	
 	#generate element list
+	
 
 	#check constraints by looking at generated element list
 	
@@ -89,55 +147,68 @@ def evaluate(params):
 	
 	
 #optimization loop
-g_OptimumParams = g_CurrParams
-fOptimum = evaluate(g_OptimumParams)
+g_dOptimumParams = dict(g_dParams)
+fOptimum = evaluate(g_dOptimumParams)
 
 bBreak = False
 bProgress = True
 
+# global step scaler
 fGlobalMultiplier = 0.5
 
 while not bBreak:
 	#if we don't make any progress, reduce step widths
 
-	bBreak = True
+	print("optimizer: global step ratio:", fGlobalMultiplier)
+
 	if not bProgress:
+		print("optimizer: no progress, cutting global multiplier down")
 		#cut step width in half
 		fGlobalMultiplier = fGlobalMultiplier + .5 * (1.0 - fGlobalMultiplier)
 		#check for minimum step width
 		
 		if (1.0 - fGlobalMultiplier) < g_fMinLenChange:
 			bBreak = True
-		else:
-			#if we aren't at minimum step width yet, we can go on
-			bBreak = False
-	
+		#if we aren't at minimum step width yet, we can go on
+		
 	bProgress = False
 	
 	#line search for one parameter
-	for param in g_OptimumParams.keys():
-		
-		for fMultiplier in [fGlobalMultiplier, 1/fGlobalMultiplier]:
-			#try moving the parameter around
-			g_CurrParams = g_OptimumParams
+	for param in g_dOptimumParams.keys():
 			
-			fParam = g_CurrParams[param]
+		# check if the global step ratio is already smaller than the allowable step ratio for the parameter
+		if (1.0 - fGlobalMultiplier) < g_dParams[param][PARAM_MIN_CHANGE]:
+			continue
+		
+		for fMultiplier in [fGlobalMultiplier, 1.0/fGlobalMultiplier]:
+			#try moving the parameter around
+			
+			# copy parameter dictionary
+			g_dSampleParams = dict()
+			for strKey in g_dOptimumParams:
+				g_dSampleParams[strKey] = list(g_dOptimumParams[strKey])
+			
+			fParam = g_dOptimumParams[param][PARAM_CURR]
 			fChangedParam = fParam * fMultiplier
+			#print(param, fParam, "->", fChangedParam)
 			
 			fDelta = abs(fParam - fChangedParam)
-			#check for minimum step width
-			if param[-2:-1] == "a":
-				if (1.0 - fGlobalMultiplier) < g_fMinAreaChange:
-					continue
-			else:
-				if fDelta < g_fMinLenStep:
-					continue
+
+			# check for minimum step width
+			if fDelta < g_dOptimumParams[param][PARAM_MIN_STEP]:
+				continue
+				
+			# check for bounds
+			fChangedParam = max(fChangedParam, g_dOptimumParams[param][PARAM_MIN])
+			fChangedParam = min(fChangedParam, g_dOptimumParams[param][PARAM_MAX])
+
+			# apply parameter change		
+			g_dSampleParams[param][PARAM_CURR] = fChangedParam
 			
-			g_CurrParams[param] = fChangedParam
-			
-			fCost = evaluate(g_CurrParams)
+			fCost = evaluate(g_dSampleParams)
 			if fCost < fOptimum:
+				print("optimizer: new optimum", g_dSampleParams)
 				bProgress = True
 				fOptimum = fCost
-				g_OptimumParams = g_CurrParams
+				g_dOptimumParams = dict(g_dSampleParams)
 
